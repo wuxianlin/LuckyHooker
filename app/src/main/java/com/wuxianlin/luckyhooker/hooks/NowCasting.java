bbf9e871c74b15d30942cbd96b391de4ec759ca1
wuxianlin <wuxianlinwxl@gmail.com>
2025-11-16 18:04:27 +0800

    update nowcasting hook

--- app/src/main/java/com/wuxianlin/luckyhooker/hooks/NowCasting.java
+++ app/src/main/java/com/wuxianlin/luckyhooker/hooks/NowCasting.java
@@ -35,53 +35,7 @@ public class NowCasting implements Hook {
         if (hookPackageNames[0].equals(lpparam.packageName)) {
             XposedBridge.hookAllMethods(XposedHelpers.findClass("com.huawei.android.sdk.drm.Drm", lpparam.classLoader), "check", XC_MethodReplacement.returnConstant(null));
         }
-        String[][] usedUserClasses = new String[][]{
-                {"com.nowcasting.activity.APPCenterActivity", "setUser", "0"},
-                {"com.nowcasting.view.MainActivityView", "setUser", "0"},
-                {"com.nowcasting.view.MainTitleView", "setUser", "0"},
-                {"com.nowcasting.viewmodel.APPCenterViewModel", "getBannerInfo", "0"},
-                {"com.nowcasting.viewmodel.APPCenterViewModel", "handleBannerStrategy", "0"},
-                {"com.nowcasting.viewmodel.APPCenterViewModel", "parseBannerInfo", "1"}
-        };
-        Method userParse = null;
-        for (String[] usedUserClass : usedUserClasses) {
-            for (Method method : HookUtils.findMethodWithName(usedUserClass[0], lpparam.classLoader,
-                    usedUserClass[1])) {
-                Class[] paramClasses = method.getParameterTypes();
-                int userIndex = Integer.parseInt(usedUserClass[2]);
-                if (paramClasses.length <= userIndex)
-                    continue;
-                Class userClass = paramClasses[userIndex];
-                if (userClass == null)
-                    continue;
-                for (Method methodInUser : userClass.getDeclaredMethods()) {
-                    Class[] paramUserClasses = methodInUser.getParameterTypes();
-                    if (paramUserClasses.length == 1 &&
-                            "org.json.JSONObject".equals(paramUserClasses[0].getName())) {
-                        userParse = methodInUser;
-                    }
-                }
-                if (userParse != null)
-                    break;
-            }
-        }
-        if (userParse == null)
-            return;
-        XposedBridge.log("finded user.parse(JsonObject) method:" +
-                userParse.getDeclaringClass().getName() + "." + userParse.getName() + "(JsonObject)");
-        XposedBridge.hookMethod(userParse, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                JSONObject jsonObject = (JSONObject) param.args[0];
-                //XposedBridge.log(jsonObject.toString('\n'));
-                long time = System.currentTimeMillis() / 1000 + 365 * 24 * 60 * 60;
-                jsonObject.put("vip_expired_at", time);
-                jsonObject.put("svip_expired_at", time);
-                jsonObject.put("vip_type", "s");
-                jsonObject.put("is_vip", true);
-                param.args[0] = jsonObject;
-            }
-        });
+        XposedBridge.hookAllMethods(XposedHelpers.findClass("com.nowcasting.entity.UserInfo", lpparam.classLoader),"isVIP", XC_MethodReplacement.returnConstant(true));
     }
 
     @Override
